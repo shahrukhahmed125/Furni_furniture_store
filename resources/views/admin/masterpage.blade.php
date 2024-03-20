1d75b6625f9c8aa03081c0cbc4631ce4e8d01cf3
Shahrukh Ahmed <shahrukhahmed125@gmail.com>
2024-03-20 23:42:22 +0500

    dashboard

--- resources/views/admin/masterpage.blade.php
+++ resources/views/admin/masterpage.blade.php
@@ -30,10 +30,10 @@
     <div class="container-scroller">
       <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        {{-- <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-          <a class="sidebar-brand brand-logo" href="index.html"><img src="admin/assets/images/logo.svg" alt="logo" /></a>
-          <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="admin/assets/images/logo-mini.svg" alt="logo" /></a>
-        </div> --}}
+        <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+          <a class="sidebar-brand brand-logo navbar-brand text-white" href="{{route('index')}}" style="text-decoration: none;"><img src="{{asset('admin/assets/images/favicon.png')}}" style="height: 30px;width:30px;" alt="logo"> Furni<span style="color: #899E96; ">.</span></a>
+          <a class="sidebar-brand brand-logo-mini text-white" href="{{route('index')}}" style=""><img src="{{asset('admin/assets/images/favicon.png')}}" style="height: 30px;width:30px;" alt="logo"></a>
+        </div>
         <ul class="nav">
           <li class="nav-item profile">
             <div class="profile-desc">
